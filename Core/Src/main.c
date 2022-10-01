92ad2e013554dd914095408367afef0dc3af2244
diyuenji <112496492+diyuenji@users.noreply.github.com>
2022-10-01 18:17:46 +0700

    ex4

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -61,21 +61,21 @@ void update7SEG ( int index ) {
 switch ( index ) {
 	case 0:
 		//HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin ,			GPIO_PIN_SET ) ;
-		HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin ,			GPIO_PIN_RESET ) ;
+		//HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin ,			GPIO_PIN_RESET ) ;
 		display7Seg(led_buffer[0]);
 		break ;
 	case 1:
 // Display the second 7 SEG with led_buffer [1]
-		HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin ,			GPIO_PIN_RESET ) ;
+		//HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin ,			GPIO_PIN_RESET ) ;
 		display7Seg(led_buffer[0]);
 		break ;
 	case 2:
  // Display the third 7 SEG with led_buffer [2]
-		HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin ,			GPIO_PIN_RESET ) ;
+		//HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin ,			GPIO_PIN_RESET ) ;
 		display7Seg(led_buffer[0]);
 		break ;
 	case 3:
-		HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin ,			GPIO_PIN_RESET ) ;
+		//HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin ,			GPIO_PIN_RESET ) ;
 		display7Seg(led_buffer[0]);
 // Display the forth 7 SEG with led_buffer [3]
 		break ;
@@ -307,10 +307,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	timerRun1();
 	timerRun2();
-	/*if(index_led<=3){
+	if(index_led<=3){
 		update7SEG(index_led++);
 	}
-	else index_led=0;*/
+	else index_led=0;
 }
 void display7Seg(int num){
 	switch(num){
